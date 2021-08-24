c03ca2a6d272ae98207befb3c34a53c405e62dea
Albert Kiehn <kiehn.albert@icloud.com>
2021-08-24 11:10:52 +0100

    model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :cars, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :cars, through: :bookings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :cars
-  has_many :bookings
 end
