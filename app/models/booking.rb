e4e54aa71fa642257b44b80438b10cb516c30546
Johannes von Foerster <jvonfoerster@gmail.com>
2021-08-24 18:27:15 +0100

    cars controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,6 @@
 class Booking < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :user
-  belongs_to :car
-=======
   belongs_to :car
   belongs_to :user
->>>>>>> master
 
   validates :start_date, :end_date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'bookings#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :cars, only: [:show, :index, :new, :create]
-=======
+
   get '/bookings', to: 'bookings#index'
   get '/cars/bookings/new', to: 'bookings#new'
   post "bookings", to: "bookings#create"
   get '/cars/bookings/:id', to: 'bookings#show'
->>>>>>> master
 end
