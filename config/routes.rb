32bca7f721cbfe6a0b6a7eaae7cf628826b98d33
Mariola Krause <mariolakrause19@gmail.com>
2021-08-25 11:05:10 +0100

    home page is ready to go

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'bookings#home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/bookings', to: 'bookings#index'
   get '/cars/bookings/new', to: 'bookings#new'
