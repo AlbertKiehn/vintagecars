d3969194baffb5ebc9c0782550e98269f8212a0f
Albert Kiehn <kiehn.albert@icloud.com>
2021-08-26 15:30:18 +0100

    thursday changes by albert

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :start_date, :end_date, presence: true
+  validates :confirmation,
+            :inclusion => { :in => [true, false] }
 end
