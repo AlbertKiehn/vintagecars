61698ad61d6914029759df0682d3339d0a85740d
Johannes von Foerster <jvonfoerster@gmail.com>
2021-08-26 15:37:10 +0100

    ownercar

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cars, only: [:show, :index, :new, :create] do
+    get :display, on: :collection
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index]
   get '/cars/bookings/:id', to: 'bookings#show'
+
 end
